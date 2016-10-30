bb027339c2c0ee815237c0b5cb5405320974cfc2
Chris Halbert <christopher.halbert@dominionenterprises.com>
2016-10-30 17:13:19 -0400

    master:  Add git wrapper.

---
+++ src/Git.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ChrisHalbert\Git;
+
+use ChrisHalbert\Git\Exception\InvalidCommandComposition;
+use ChrisHalbert\Git\Utility\Inflector;
+use ChrisHalbert\Git\Utility\Stringify;
+
+class Git
+{
+    const SUCCESS = 0;
+
+    public function __call($method, $args)
+    {
+        $command = Inflector::camelToChain($method);
+        return $this->execute($command, $args);
+    }
+
+    public function blame($file, $line)
+    {
+        $blameCommand = sprintf("-L%s,%s --line-porcelain -- %s", $line, $line, $file);
+        return $this->command('blame', $blameCommand);
+    }
+
+    /**
+     * @param $command
+     * @param $args
+     */
+    private function command($command, $args)
+    {
+        $command = sprintf('git %s %s', $command, Stringify::arguments($args));
+        list($output, $return) = $this->execute($command);
+        if ($return != self::SUCCESS) {
+            throw new InvalidCommandComposition();
+        }
+
+        return $output;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function execute($command)
+    {
+        exec($command, $output, $return);
+        return [$output, $return];
+    }
+}
